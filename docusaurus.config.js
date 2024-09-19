3fede786462be0516c65c8bfac8ff4128942f642
ning <ning.huang.ridson@gmail.com>
2024-09-19 15:30:22 +0800

    algolia search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,6 +9,8 @@ const config = {
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
+  // themes: ['@docusaurus/theme-search-algolia'],
+
   // Set the production url of your site here
   url: 'https://Arterning.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
@@ -95,6 +97,38 @@ const config = {
             position: 'right',
           },
         ],
+      },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'AV2951DRWO',
+  
+        // Public API key: it is safe to commit it
+        apiKey: '82ed9bb2d41fd36dd910bec7131a3ec2',
+  
+        indexName: 'arterningio',
+  
+        // Optional: see doc section below
+        contextualSearch: true,
+  
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: 'external\\.com|domain\\.com',
+  
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: '/docs/', // or as RegExp: /\/docs\//
+          to: '/',
+        },
+  
+        // Optional: Algolia search parameters
+        searchParameters: {},
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+  
+        // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+        insights: false,
+  
+        //... other Algolia params
       },
       footer: {
         style: 'dark',
